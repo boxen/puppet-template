15d0b849d5db65509c2649048d4c2d5c01c2f8d4
Johan Haals <johan.haals@gmail.com>
2013-04-03 09:49:59 +0200

    Spec instructions

--- spec/classes/template_spec.rb
+++ spec/classes/template_spec.rb
@@ -1,5 +1,7 @@
 require 'spec_helper'
-
+# Rename this file to classname_spec.rb
+# Check other boxen modules for examples
+# or read http://rspec-puppet.com/tutorial/
 describe 'template' do
     it do
       should contain_notify('Hello_World')
